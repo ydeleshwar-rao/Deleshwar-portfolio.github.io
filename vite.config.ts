e47587d7e4df669dd91880914b95fc88f81cbac6
Y-Deleshwar <y.deleshwar.rao@nirvanagroup.co.in>
2025-07-22 18:34:55 +0530

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: [
-     'determination-reduce-funky-genome.trycloudflare.com'
-    ]
-  }
+   base: '/Deleshwar-portfolio.github.io/',
+  // server: {
+  //   allowedHosts: [
+  //    'determination-reduce-funky-genome.trycloudflare.com'
+  //   ]
+  // }
 })
